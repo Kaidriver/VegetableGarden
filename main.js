6d8fd6be51686412894583864bd30ef0664db1bd
Kaiwen Song <jirachiji@gmail.com>
2021-04-09 21:37:43 -0400

    final update before launch

--- main.js
+++
@@ -1,8 +0,0 @@
-
-function initMap() {
-
-  var resturant = {lat: 39.103008, lng: -77.074977}
-  var map = new google.maps.Map(document.getElementById('map'), {zoom: 15, center: resturant})
-  var marker = new google.maps.Marker({position: resturant, map: map})
-
-}
